8e5f996687382b668103b6a9cfa3a8a899f9dc8a
Mortron <cmsl4y3r@gmail.com>
2024-10-14 20:10:00 -0600

    asteroids spawn from larger dead ones, but need to move away from them now

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -12,7 +12,8 @@ public class DestroyOnCollision : MonoBehaviour
     public int enemyTypeRange;
     void OnTriggerEnter(Collider other)
     {
-        if (tag != "EnemyLaser" && tag != "Repair" && tag != "PowerUp")
+        //if (tag != "EnemyLaser" && tag != "Repair" && tag != "PowerUp")
+        if (other.CompareTag("PlayerWeapon") || other.CompareTag("Player"))
         {
             string objectName = gameObject.name;
             int idx = Random.Range(expBase, enemyTypeRange);
@@ -20,32 +21,34 @@ public class DestroyOnCollision : MonoBehaviour
             
             Destroy(other.gameObject);
             
-            Debug.Log("objectName: " + objectName); 
+            Debug.Log("objectName: " + objectName);
+            Vector3 offset1 = new Vector3(1, 0, 1);
+            Vector3 offset2 = new Vector3(-1, 0, -1);
             switch (objectName)
             {
                 case "asteroidLarge_1(Clone)":
-                    GameObject medRock1 = Instantiate(medAsteroid, transform.position, transform.rotation); 
-                    GameObject smallRock2 = Instantiate(smallAsteroid, transform.position, transform.rotation);
+                    GameObject medRock1 = Instantiate(medAsteroid, transform.position + offset1, transform.rotation);
+                    GameObject smallRock2 = Instantiate(smallAsteroid, transform.position + offset2, transform.rotation);
                     // Apply force to blow them apart
                     Rigidbody rb1 = medRock1.GetComponent<Rigidbody>();
                     Rigidbody rb2 = smallRock2.GetComponent<Rigidbody>();
-                    rb1.AddForce(new Vector3(1, 0, 1) * 5f, ForceMode.Impulse);  // Push in random directions
-                    rb2.AddForce(new Vector3(-1, 0, -1) * 5f, ForceMode.Impulse);
+                    rb1.AddForce(new Vector3(1, 0, 1) * 150f, ForceMode.Impulse);  // Push in random directions
+                    rb2.AddForce(new Vector3(-1, 0, -1) * 150f, ForceMode.Impulse);
                     break;
                 case "asteroidMed_2(Clone)":
-                    GameObject smallRock3 = Instantiate(smallAsteroid, transform.position, transform.rotation);
-                    GameObject smallRock4 = Instantiate(smallAsteroid, transform.position, transform.rotation);
+                    GameObject smallRock3 = Instantiate(smallAsteroid, transform.position + offset1, transform.rotation);
+                    GameObject smallRock4 = Instantiate(smallAsteroid, transform.position + offset2, transform.rotation);
                     // Apply force to blow them apart
                     Rigidbody rb3 = smallRock3.GetComponent<Rigidbody>();
                     Rigidbody rb4 = smallRock4.GetComponent<Rigidbody>();
-                    rb3.AddForce(new Vector3(1, 0, -1) * 5f, ForceMode.Impulse);
-                    rb4.AddForce(new Vector3(-1, 0, 1) * 5f, ForceMode.Impulse);
+                    rb3.AddForce(new Vector3(1, 0, -1) * 150f, ForceMode.Impulse);
+                    rb4.AddForce(new Vector3(-1, 0, 1) * 150f, ForceMode.Impulse);
                     break;
                 default:
                     break;
             }
 
-            Destroy(gameObject, 0.1f);
+            Destroy(gameObject);
 
         }
     }
